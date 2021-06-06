0936da5f6df96f60affa0a29b7070ea3e618ff4d
Antonio Mavaro <amavaro@gmail.com>
2021-06-06 11:28:51 +0200

    Add countdown script to under-construction page

--- under-construction/script.js
+++ under-construction/script.js
@@ -1,7 +1,7 @@
-let countDownDate = new Date("May 6, 2022 00:00:00").getTime;
+let countDownDate = new Date("Jun 7, 2021 00:00:00").getTime();
 
 let x = setInterval(function(){
-let now = new.Date().getTime();
+let now = new Date().getTime();
 let distance = countDownDate - now;
 
 let days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -9,11 +9,11 @@ let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-document.getElementById("demo").innerHTML = "Paragraph changed!";
+document.getElementById("demo").innerHTML = days + "g " + hours + "o " + minutes + "m " + seconds + "s ";
 
 if(distance < 0) {
   clearInterval(x);
   document.getElementById("demo").innerHTML("CONTO ALLA ROVESCIA FINITO!")
 }
 
-}, 1000)
+}, 1000);
